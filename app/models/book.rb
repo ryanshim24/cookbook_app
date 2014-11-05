3f0233ba4763302cfaefbcfb8be37d880424b696
ryanshim24 <ryanshim24@yahoo.com>
2014-11-05 15:30:36 -0800

    Added given things

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ActiveRecord::Base
   has_many :recipes
+  validates :title, presence: true, length: { 
+    minimum: 3,
+    maximum: 100,
+    too_short: "must have least %{count} letters, dude",
+    too_long: "is way too long"
+}
 end
